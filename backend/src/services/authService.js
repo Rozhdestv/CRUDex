d05c356ee4d15e838cdef5de9a8b76f2bbbe6f48
Rozhdestv <82630955+Rozhdestv@users.noreply.github.com>
2026-06-17 23:05:00 -0500

    gitignore

--- backend/src/services/authService.js
+++ backend/src/services/authService.js
@@ -4,7 +4,7 @@ const usuarioRepository = require("../repositories/usuarioRepository");
 
 async function login(username, password) {
   const user = await usuarioRepository.findByUsername(username);
-  console.log("Usuario encontrado:", user); // 👈 Agrega esto
+  console.log("Usuario encontrado:", user);
   if (!user) return { success: false, error: "Credenciales inválidas" };
   if (!user.activo) return { success: false, error: "Usuario deshabilitado" };
   const valid = await bcrypt.compare(password, user.password_hash);
